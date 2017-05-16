49422d37ea50e52e3beec97b3cd79544a532a4b6
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2017-05-16 00:18:27 -0300

    [Perl/HashSort] Implementação da subrotina 'max_heapify'

--- Perl/Heapsort/lib/HeapSort.pm
+++ Perl/Heapsort/lib/HeapSort.pm
@@ -3,9 +3,49 @@ package HeapSort;
 use strict;
 use warnings;
 
+
 sub sort {
     my @input = @_;
     return @input;
 }
+
+
+sub max_heapify {
+    my ( $heap_array, $node_index ) = @_;
+    my ( $array_index, $left_index, $right_index ) = ( $node_index - 1, 2 * $node_index - 1, 2 * $node_index);
+    my $biggest_value_index = $array_index;
+
+    $biggest_value_index = index_with_biggest_value( $heap_array, $biggest_value_index, $left_index );
+    $biggest_value_index = index_with_biggest_value( $heap_array, $biggest_value_index, $right_index );
+
+    if( $biggest_value_index != $array_index ) {
+        switch_nodes( $heap_array, $biggest_value_index, $array_index );
+        return max_heapify( $heap_array, $biggest_value_index + 1 );
+    }
+
+    return @{ $heap_array };
+}
+
+
+sub index_with_biggest_value {
+    my ( $array, $parent_index, $child_index ) = @_;
+    my $array_length = $#{$array};
+    my $biggest_value_index = $parent_index;
+
+    if( $child_index <= $array_length &&
+        $array->[$child_index] > $array->[$parent_index] ) {
+        $biggest_value_index = $child_index;
+    }
+    return $biggest_value_index;
+}
+
+
+sub switch_nodes {
+    my ( $array, $first_index, $second_index ) = @_;
+    my $tmp = $array->[$first_index];
+    $array->[$first_index] = $array->[$second_index];
+    $array->[$second_index] = $tmp;
+}
+
 
 return 1;
--- Perl/Heapsort/tests/algorithm_tests.pl
+++ Perl/Heapsort/tests/algorithm_tests.pl
@@ -12,15 +12,60 @@ use HeapSort;
 
 
 subtest "ordenação de array vazio" => sub {
-    my @expected = ();
     my @actual = HeapSort::sort( () );
-    ok( is_deeply( \@actual, \@expected ) );
+    ok( is_deeply( \@actual, [] ) );
 };
 
 subtest "ordenação de array com um único elemento" => sub {
-    my @expected = (9);
     my @actual = HeapSort::sort( (9) );
-    ok( is_deeply( \@actual, \@expected ) );
+    ok( is_deeply( \@actual, [9] ) );
+};
+
+subtest "ordenação de array ordenado com dois elementos" => sub {
+    my @actual = HeapSort::sort( (3, 9) );
+    ok( is_deeply( \@actual, [3, 9] ) );
+};
+
+subtest "ordena heap para heap de tamanho 1" => sub {
+    my @actual = HeapSort::max_heapify( [23], 1 );
+    ok( is_deeply( \@actual, [23] ) );
+};
+
+subtest "ordena heap de tamanho 2" => sub {
+    my $expected = [23, 11];
+
+    my @actual = HeapSort::max_heapify( [23, 11], 1 );
+    ok( is_deeply( \@actual, $expected ) );
+
+    @actual = HeapSort::max_heapify( [11, 23], 1 );
+    ok( is_deeply( \@actual, $expected ) );
+};
+
+subtest "ordena heap de tamanho 3" => sub {
+    my $expected = [4, 3, 2];
+
+    my @actual = HeapSort::max_heapify( [4, 3, 2], 1 );
+    ok( is_deeply( \@actual, $expected ) );
+
+    @actual = HeapSort::max_heapify( [3, 4, 2], 1 );
+    ok( is_deeply( \@actual, $expected ) );
+
+    @actual = HeapSort::max_heapify( [2, 3, 4], 1 );
+    ok( is_deeply( \@actual, $expected ) );
+};
+
+subtest "ordena heap de tamanho 6" => sub {
+    my @actual = HeapSort::max_heapify( [23, 19, 14, 2, 9, 5], 1 );
+    ok( is_deeply( \@actual, [23, 19, 14, 2, 9, 5] ) );
+
+    @actual = HeapSort::max_heapify( [19, 23, 14, 2, 9, 5], 1 );
+    ok( is_deeply( \@actual, [23, 19, 14, 2, 9, 5] ) );
+
+    @actual = HeapSort::max_heapify( [2, 19, 23, 14, 9, 5], 1 );
+    ok( is_deeply( \@actual, [23, 19, 5, 14, 9, 2] ) );
+
+    @actual = HeapSort::max_heapify( [9, 23, 19, 14, 2, 5], 1 );
+    ok( is_deeply( \@actual, [23, 14, 19, 9, 2, 5] ) );
 };
 
 done_testing();
